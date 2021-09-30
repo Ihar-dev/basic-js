d0b42179631737742c5573cb5f4f3430e6f4d7c5
Ihar Kozik <ihar.kozik@gmail.com>
2021-10-01 01:25:20 +0300

    feat: edit st-encode-line.js

--- src/st-encode-line.js
+++ src/st-encode-line.js
@@ -1,4 +1,6 @@
-import { NotImplementedError } from '../extensions/index.js';
+import {
+  NotImplementedError
+} from '../extensions/index.js';
 
 /**
  * Given a string, return its encoding version.
@@ -10,7 +12,15 @@ import { NotImplementedError } from '../extensions/index.js';
  * For aabbbc should return 2a3bc
  *
  */
-export default function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function encodeLine(str) {
+  let output = '';
+  let num = 1;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) num++;
+    else {
+      (num === 1) ? output += str[i]: output += num + str[i];
+      num = 1;
+    }
+  }
+  return output;
 }
