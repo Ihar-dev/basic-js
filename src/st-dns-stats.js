2bc06cea75dc007b26d1b3efbcd566294d8e915e
Ihar Kozik <ihar.kozik@gmail.com>
2021-10-01 01:09:23 +0300

    feat: edit st-dns-stats.js

--- src/st-dns-stats.js
+++ src/st-dns-stats.js
@@ -1,4 +1,6 @@
-import { NotImplementedError } from '../extensions/index.js';
+import {
+  NotImplementedError
+} from '../extensions/index.js';
 
 /**
  * Given an array of domains, return the object with the appearances of the DNS.
@@ -22,7 +24,20 @@ import { NotImplementedError } from '../extensions/index.js';
  * }
  *
  */
-export default function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getDNSStats(domains) {
+  const arr = [];
+  let tempArr = [];
+  let str = '';
+  for (let i in domains) {
+    tempArr.push(domains[i].split('.'));
+    for (let j = tempArr[0].length - 1; j >= 0; j--) {
+      str += '.' + tempArr[0][j];
+      arr.push(str);
+    }
+    str = '';
+    tempArr = [];
+  }
+  let output = {};
+  for (let k in arr)(output[arr[k]]) ? output[arr[k]]++ : output[arr[k]] = 1;
+  return output;
 }
